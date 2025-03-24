9fe4e9ea6a3ed94afbfb35e82e9530f3cc4619e5
Diluth Rangana <138687673+diluthrangana@users.noreply.github.com>
2025-03-24 12:05:55 +0530

    server updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const stripe = Stripe("sk_test_51R5skGGYABQw6E1f9QbbUNrAJJOn9cXt4cjjJ3pUaKNLHwDKXl4ziuyL1yrBRUCkiiKpYDX1Knt3v5d1sx68Ljuz001eSEZ7gM"); // Use ENV variable
+const stripe = Stripe("pk_test_51R64TNP6W0yfsGrVwwpRz0orP5XgMIjYI6EeFWGAYlPBqaVOhfNzwLB1Bor9rKu3nZmT2pbnbWmOobuKZwwc2MOy00kyguhOTp"); // Use ENV variable
 
 app.post("/create-payment-intent", async (req, res) => {
   try {
